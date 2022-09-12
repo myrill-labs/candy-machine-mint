b1a0dc38a4158bccbb0c4f2ab880d505c159064b
nicolas carrara <nicolas.carrara@myrill.io>
2022-09-12 22:17:14 +0200

    removing everything

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,30 +1,8 @@
-import {ReactElement, useEffect, useState} from "react";
+import {ReactElement, useEffect} from "react";
 import styled from "styled-components";
-import Countdown from "react-countdown";
-import {Button, CircularProgress, Grid, Snackbar} from "@material-ui/core";
-import Alert from "@material-ui/lab/Alert";
+import {Button, Link} from "@material-ui/core";
 import * as anchor from "@project-serum/anchor";
-import {LAMPORTS_PER_SOL} from "@solana/web3.js";
-import {useAnchorWallet} from "@solana/wallet-adapter-react";
-import {WalletDialogButton} from "@solana/wallet-adapter-material-ui";
 
-import {
-    CandyMachine,
-    awaitTransactionSignatureConfirmation,
-    getCandyMachineState,
-    mintOneToken,
-    shortenAddress,
-} from "./candy-machine";
-
-const ConnectButton = styled(WalletDialogButton)``;
-
-const CounterText = styled.span``; // add your styles here
-
-const MintContainer = styled.div``; // add your styles here
-
-const MintButton = styled(Button)`
-
-`; // add your styles here
 
 export interface HomeProps {
     candyMachineId: anchor.web3.PublicKey;
@@ -36,133 +14,6 @@ export interface HomeProps {
 }
 
 const Home = (props: HomeProps) => {
-    const [balance, setBalance] = useState<number>();
-    const [isActive, setIsActive] = useState(false); // true when countdown completes
-    const [isSoldOut, setIsSoldOut] = useState(false); // true when items remaining is zero
-    const [isMinting, setIsMinting] = useState(false); // true when user got to press MINT
-
-    const [itemsAvailable, setItemsAvailable] = useState(0);
-    const [itemsRedeemed, setItemsRedeemed] = useState(0);
-    const [itemsRemaining, setItemsRemaining] = useState(0);
-
-    const [alertState, setAlertState] = useState<AlertState>({
-        open: false,
-        message: "",
-        severity: undefined,
-    });
-
-    const [startDate, setStartDate] = useState(new Date(props.startDate));
-
-    const wallet = useAnchorWallet();
-    const [candyMachine, setCandyMachine] = useState<CandyMachine>();
-
-    const refreshCandyMachineState = () => {
-        (async () => {
-            if (!wallet) return;
-
-            const {
-                candyMachine,
-                goLiveDate,
-                itemsAvailable,
-                itemsRemaining,
-                itemsRedeemed,
-            } = await getCandyMachineState(
-                wallet as anchor.Wallet,
-                props.candyMachineId,
-                props.connection
-            );
-
-            setItemsAvailable(itemsAvailable);
-            setItemsRemaining(itemsRemaining);
-            setItemsRedeemed(itemsRedeemed);
-
-            setIsSoldOut(itemsRemaining === 0);
-            setStartDate(goLiveDate);
-            setCandyMachine(candyMachine);
-        })();
-    };
-
-    const onMint = async () => {
-        try {
-            setIsMinting(true);
-            if (wallet && candyMachine?.program) {
-                const mintTxId = await mintOneToken(
-                    candyMachine,
-                    props.config,
-                    wallet.publicKey,
-                    props.treasury
-                );
-
-                const status = await awaitTransactionSignatureConfirmation(
-                    mintTxId,
-                    props.txTimeout,
-                    props.connection,
-                    "singleGossip",
-                    false
-                );
-
-                if (!status?.err) {
-                    setAlertState({
-                        open: true,
-                        message: "Congratulations! Mint succeeded!",
-                        severity: "success",
-                    });
-                } else {
-                    setAlertState({
-                        open: true,
-                        message: "Mint failed! Please try again!",
-                        severity: "error",
-                    });
-                }
-            }
-        } catch (error: any) {
-            // TODO: blech:
-            let message = error.msg || "Minting failed! Please try again!";
-            if (!error.msg) {
-                if (error.message.indexOf("0x138")) {
-                } else if (error.message.indexOf("0x137")) {
-                    message = `SOLD OUT!`;
-                } else if (error.message.indexOf("0x135")) {
-                    message = `Insufficient funds to mint. Please fund your wallet.`;
-                }
-            } else {
-                if (error.code === 311) {
-                    message = `SOLD OUT!`;
-                    setIsSoldOut(true);
-                } else if (error.code === 312) {
-                    message = `Minting period hasn't started yet.`;
-                }
-            }
-
-            setAlertState({
-                open: true,
-                message,
-                severity: "error",
-            });
-        } finally {
-            if (wallet) {
-                const balance = await props.connection.getBalance(wallet.publicKey);
-                setBalance(balance / LAMPORTS_PER_SOL);
-            }
-            setIsMinting(false);
-            refreshCandyMachineState();
-        }
-    };
-
-    useEffect(() => {
-        (async () => {
-            if (wallet) {
-                const balance = await props.connection.getBalance(wallet.publicKey);
-                setBalance(balance / LAMPORTS_PER_SOL);
-            }
-        })();
-    }, [wallet, props.connection]);
-
-    useEffect(refreshCandyMachineState, [
-        wallet,
-        props.candyMachineId,
-        props.connection,
-    ]);
 
     const ColorButton = styled(Button)(({theme}) => ({
 
@@ -174,14 +25,6 @@ const Home = (props: HomeProps) => {
         },
     }));
 
-    const MenuButton = styled(Button)(({theme}) => ({
-        color: "white",
-        backgroundColor: "none",
-        '&:hover': {
-            backgroundColor: "pink",
-            color: "rgba(100, 100, 255, 0.8)"
-        },
-    }));
 
     const SolIcon: React.FC = (): ReactElement => {
         return <img
@@ -193,51 +36,22 @@ const Home = (props: HomeProps) => {
         document.title = "Join Myrill"
     }, []);
 
-    // @ts-ignore
-    function myFunction() {
-        let x = document.getElementById("myTopnav");
-        // @ts-ignore
-        if (x.className === "topnav") {
-            // @ts-ignore
-            x.className += " responsive";
-        } else {
-            // @ts-ignore
-            x.className = "topnav";
-        }
-    }
+
+    const onMint = () => {
+    };
 
     return (
         <main>
 
-            {/*<div className="space-30"/>*/}
-
-
-            <div style={{"margin": "15px"}}>
-
-
-                <div className={"walletDiv"}>
-
-                    {wallet && (
-                        <p>
-                            Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}
-                            <div style={{"marginRight": "10px"}}/>
-                            <SolIcon/> {(balance || 0).toLocaleString()}
-
-                        </p>
-                    )}
-                </div>
-            </div>
 
             <div className="space-30"/>
 
 
             <div className={"mintDiv"}>
                 <div className="space-30"/>
-                <div style={{margin: "15px",justifyContent: 'center', alignItems: 'center'}}>
+                <div style={{margin: "15px", justifyContent: 'center', alignItems: 'center'}}>
                     This NFT unlocks a right to deposit the equivalent of $10k of liquidity to the Myrill protocol.
-
                     <br/><br/>
-
                     Note that this is a pre-sale and the protocol is still in R&D.
                 </div>
                 <br/>
@@ -248,80 +62,21 @@ const Home = (props: HomeProps) => {
                     alt="NFT"/>
 
                 <div className="space-10"/>
-
-
                 <div>
-
-
-                    {wallet && <p>Total Available: {itemsAvailable}</p>}
-
-                    {wallet && <p>Redeemed: {itemsRedeemed}</p>}
-
-                    {wallet && <p>Remaining: {itemsRemaining}</p>}
-
                     <p>price: <SolIcon/> 5 </p>
-
-                    <MintContainer>
-                        {!wallet ? (
-                            <ConnectButton>Connect Wallet</ConnectButton>
-                        ) : (
-                            <ColorButton
-                                disabled={isSoldOut || isMinting || !isActive}
-                                onClick={onMint}
-                                variant="contained"
-                            >
-                                {isSoldOut ? (
-                                    "SOLD OUT"
-                                ) : isActive ? (
-                                    isMinting ? (
-                                        <CircularProgress/>
-                                    ) : (
-                                        "BUY NFT"
-                                    )
-                                ) : (
-                                    <Countdown
-                                        date={startDate}
-                                        onMount={({completed}) => completed && setIsActive(true)}
-                                        onComplete={() => setIsActive(true)}
-                                        renderer={renderCounter}
-                                    />
-                                )}
-                            </ColorButton>
-                        )}
-                    </MintContainer>
-
-                    <Snackbar
-                        open={alertState.open}
-                        autoHideDuration={6000}
-                        onClose={() => setAlertState({...alertState, open: false})}
-                    >
-                        <Alert
-                            onClose={() => setAlertState({...alertState, open: false})}
-                            severity={alertState.severity}
-                        >
-                            {alertState.message}
-                        </Alert>
-                    </Snackbar>
+                    <Link href={"https://myrill.io/discord"}>
+                        <ColorButton
+                            onClick={onMint}
+                            variant="contained">
+                            PRIVATE SALE ARE NOW ON DISCORD
+                        </ColorButton>
+                    </Link>
                     <div className="space-30"/>
                 </div>
 
             </div>
             <div className="space-30"/>
         </main>
-    );
-};
-
-interface AlertState {
-    open: boolean;
-    message: string;
-    severity: "success" | "info" | "warning" | "error" | undefined;
-}
-
-const renderCounter = ({days, hours, minutes, seconds, completed}: any) => {
-    return (
-        <CounterText>
-            {hours + (days || 0) * 24} hours, {minutes} minutes, {seconds} seconds
-        </CounterText>
     );
 };
 
